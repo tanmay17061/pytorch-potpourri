730afcbd59555a15b73bddca9d8a03f2106d65f7
Tanmay Garg <tanmay17061@iiitd.ac.in>
2023-04-15 21:45:30 +0530

    add BatchAccumulatedMetricsCallback lightning callback

--- src/potpourri/callbacks/lightning.py
+++ src/potpourri/callbacks/lightning.py
@@ -104,3 +104,73 @@ class DataMapsCallback(pl.callbacks.Callback):
         TBD.
         """
         return self.confidence, self.variability, self.correctness
+
+class BatchAccumulatedMetricsCallback(pl.callbacks.Callback):
+    """
+    A pytorch lightning callback to help capture true metrics across an epoch of train/validation run.
+
+    Args:
+
+            NOTE: To use this callback, the user must modify their LightningModule's train/validation/test steps
+            to return the true_labels and pred_labels, both of type: `np.ndarray` and dimension: (batch_size,).
+    
+    -------------------
+
+        from lightning.pytorch import Trainer
+        from lightning.pytorch.callbacks import ModelCheckpoint, EarlyStopping
+
+        trainer_callbacks = [
+            ModelCheckpoint(
+                ...
+                monitor="val_accumulated_macro_f1",
+                mode="max",
+                ...
+            ),
+            EarlyStopping(
+                ...
+                monitor="val_accumulated_macro_f1",
+                mode="max",
+                ...
+            ),
+            BatchAccumulatedMetricsCallback(
+                metric_to_function_dict = {
+                    "accumulated_macro_f1": (f1_score,{'average':'macro'}), 
+                    "accumulated_weighted_macro_f1": (f1_score,{'average':'weighted'}),
+                }
+            ),
+        ]
+        trainer = Trainer(callbacks=trainer_callbacks, ...)
+
+    -------------------
+    This example tracks `val_accumulated_macro_f1` metric for model checkpointing and early stopping. NOTE: You can
+    refer to (https://medium.com/@tanmay17061/a7077ef8e55d) to understand why this might be important.
+    (However, The example in the blog post does not support `metric_to_function_dict` to make things more readable for a lesser technical audience.)
+    """
+    def __init__(self, metric_to_function_dict):
+        self.metric_to_function_dict = metric_to_function_dict
+
+    def on_train_epoch_start(self, trainer, pl_module):
+        self.train_actual_labels_npy = np.empty(shape=(0,), dtype=int)
+        self.train_pred_labels_npy = np.empty(shape=(0,), dtype=int)
+
+    def on_train_epoch_end(self, trainer, pl_module):
+        for m,fn_args in self.metric_to_function_dict.items():
+            fn,args = fn_args
+            pl_module.log("train_" + m, fn(self.train_actual_labels_npy,self.train_pred_labels_npy,**args))
+
+    def on_train_batch_end(self, trainer, pl_module, outputs, batch, batch_idx, dataloader_idx=0):
+        self.train_actual_labels_npy = np.concatenate((self.train_actual_labels_npy,outputs["actual_labels"],))
+        self.train_pred_labels_npy = np.concatenate((self.train_pred_labels_npy,outputs["pred_labels"],))
+
+    def on_validation_start(self, trainer, pl_module):
+        self.val_actual_labels_npy = np.empty(shape=(0,), dtype=int)
+        self.val_pred_labels_npy = np.empty(shape=(0,), dtype=int)
+
+    def on_validation_epoch_end(self, trainer, pl_module):
+        for m,fn_args in self.metric_to_function_dict.items():
+            fn,args = fn_args
+            pl_module.log("val_" + m, fn(self.val_actual_labels_npy,self.val_pred_labels_npy,**args))
+
+    def on_validation_batch_end(self, trainer, pl_module, outputs, batch, batch_idx, dataloader_idx=0):
+        self.val_actual_labels_npy = np.concatenate((self.val_actual_labels_npy,outputs["actual_labels"],))
+        self.val_pred_labels_npy = np.concatenate((self.val_pred_labels_npy,outputs["pred_labels"],))
